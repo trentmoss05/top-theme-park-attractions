6486b52bcfd92836dedb58f8c247ab37beb8d881
Trent Moss <trentmoss05@gmail.com>
2019-02-06 15:57:42 +0000

    cleaned up some some of the methods in cli class

--- lib/top_theme_park_attractions/cli.rb
+++ lib/top_theme_park_attractions/cli.rb
@@ -7,7 +7,7 @@ class TopThemeParkAttractions::CLI
   end
 
   def start
-    puts "Which attractions would you like to see? (please enter 1-20, 21-40, 41-60, 61-80, or 81-100)"
+    puts "Which attractions would you like to see? (enter a number 1-20, 21-40, 41-60, 61-80, or 81-100)"
     input = gets.strip.to_i
 
     show_attractions(input)
@@ -34,12 +34,16 @@ class TopThemeParkAttractions::CLI
   end
 
   def more_info(attraction)
-    puts "#{attraction.name} in #{attraction.theme_park} - #{attraction.position}"
+    puts "#{attraction.name} in #{attraction.theme_park.strip}"
+    puts ""
     puts "Type of Attraction:         #{attraction.ride_type}"
+    puts ""
     puts "Park URL:                   #{attraction.park_url}"
+    puts ""
     puts "Attraction Opening Date:    #{attraction.opening_date}"
+    puts ""
     puts "Description of Attraction: #{attraction.description}"
-
+    puts ""
   end
 
   def goodbye
@@ -49,7 +53,8 @@ class TopThemeParkAttractions::CLI
   def show_attractions(numbers)
     puts "Attractions #{numbers} - #{numbers+19}"
     TopThemeParkAttractions::Attractions.all[numbers-1, 20].each.with_index(numbers) do |attraction, index|
-      puts "#{index}. #{attraction.name} in #{attraction.theme_park}"
+      puts "#{index}. #{attraction.name} in #{attraction.theme_park.strip}"
+      puts ""
     end
   end
 
